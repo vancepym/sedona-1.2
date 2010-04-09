3c16a054df697308444554d78eb9f44298acf674
Craig Gemmill <Craig Gemmill@localhost>
2010-04-09 12:08:36 -0400

    Fix Niagara side DASP bytes header parsing (for ackMore)

--- src/sedona/src/sedona/dasp/DaspMessage.java
+++ src/sedona/src/sedona/dasp/DaspMessage.java
@@ -6,7 +6,7 @@
 //   10 Mar 08  Brian Frank  Creation
 //
 
-package sedona.dasp;      
+package sedona.dasp;
 
 import java.io.*;
 import java.net.*;
@@ -18,12 +18,12 @@ import sedona.util.TextUtil;
  */
 public class DaspMessage
   implements DaspConst
-{                        
+{
 
 ////////////////////////////////////////////////////////////////
-// Public Access 
+// Public Access
 ////////////////////////////////////////////////////////////////
-  
+
   /**
    * Get the session this message was received from.
    */
@@ -32,15 +32,15 @@ public class DaspMessage
   /**
    * Get the payload of the message.
    */
-  public byte[] payload() { return payload; }   
-  
+  public byte[] payload() { return payload; }
+
   /**
    * String representation.
    */
-  public String toString() 
-  { 
-    return "DaspMessage @ " + session + " (" + payload.length + " bytes)"; 
-  }  
+  public String toString()
+  {
+    return "DaspMessage @ " + session + " (" + payload.length + " bytes)";
+  }
 
 ////////////////////////////////////////////////////////////////
 // Header Field Access
@@ -59,7 +59,7 @@ public class DaspMessage
   }
 
   String digestAlgorithm()
-  {            
+  {
     if (digestAlgorithm == null) return "SHA-1";
     return digestAlgorithm;
   }
@@ -105,7 +105,7 @@ public class DaspMessage
     if (receiveTimeout == -1) return RECEIVE_TIMEOUT_DEF;
     return receiveTimeout;
   }
-  
+
 ////////////////////////////////////////////////////////////////
 // Encode
 ////////////////////////////////////////////////////////////////
@@ -114,14 +114,14 @@ public class DaspMessage
   {
     int pos = 0;
     pos = u2(pos, buf, sessionId);
-    pos = u2(pos, buf, seqNum); 
+    pos = u2(pos, buf, seqNum);
     pos++;  // ctrl byte
-    
+
     boolean hasIdealMax       = idealMax       != -1 && idealMax       != IDEAL_MAX_DEF;
     boolean hasAbsMax         = absMax         != -1 && absMax         != ABS_MAX_DEF;
     boolean hasReceiveMax     = receiveMax     != -1 && receiveMax     != RECEIVE_MAX_DEF;
     boolean hasReceiveTimeout = receiveTimeout != -1 && receiveTimeout != RECEIVE_TIMEOUT_DEF;
-    
+
     int num = 0;
     if (version != -1)           { num++; buf[pos++] = (byte)VERSION;          pos = u2(pos, buf, version); }
     if (remoteId != -1)          { num++; buf[pos++] = (byte)REMOTE_ID;        pos = u2(pos, buf, remoteId); }
@@ -136,11 +136,11 @@ public class DaspMessage
     if (hasReceiveMax)           { num++; buf[pos++] = (byte)RECEIVE_MAX;      pos = u2(pos, buf, receiveMax); }
     if (hasReceiveTimeout)       { num++; buf[pos++] = (byte)RECEIVE_TIMEOUT;  pos = u2(pos, buf, (int)(receiveTimeout/1000L)); }
     if (errorCode != -1)         { num++; buf[pos++] = (byte)ERROR_CODE;       pos = u2(pos, buf, errorCode); }
-    
-    buf[4] = (byte)((msgType << 4) | num); 
-    
+
+    buf[4] = (byte)((msgType << 4) | num);
+
     if (payload != null)
-    {                  
+    {
       System.arraycopy(payload, 0, buf, pos, payload.length);
       pos += payload.length;
     }
@@ -156,21 +156,21 @@ public class DaspMessage
   }
 
   private int bytes(int pos, byte[] buf, byte[] val)
-  { 
+  {
     buf[pos++] = (byte)val.length;
     System.arraycopy(val, 0, buf, pos, val.length);
     return pos+val.length;
   }
 
-  private int str(int pos, byte[] buf, String val)                
-  { 
+  private int str(int pos, byte[] buf, String val)
+  {
     for (int i=0; i<val.length(); ++i)
     {
       int c = val.charAt(i);
       if (c > 0x7f) throw new RuntimeException("String must be ASCII " + val);
       buf[pos++] = (byte)c;
-    }       
-    buf[pos++] = 0;               
+    }
+    buf[pos++] = 0;
     return pos;
   }
 
@@ -178,19 +178,19 @@ public class DaspMessage
 // Decode
 ////////////////////////////////////////////////////////////////
 
-  DaspMessage decode(byte[] buf, int len) 
+  DaspMessage decode(byte[] buf, int len)
     throws Exception
-  {                 
+  {
     // 5 byte header
     this.sessionId = ((buf[0] & 0xff) << 8) | (buf[1] & 0xff);
     this.seqNum    = ((buf[2] & 0xff) << 8) | (buf[3] & 0xff);
-    this.msgType   = (buf[4] & 0xff) >> 4;    
+    this.msgType   = (buf[4] & 0xff) >> 4;
     int numFields  = buf[4] & 0xf;
 
     // header fields
     int pos = 5;
     for (int i=0; i<numFields; ++i)
-    {                       
+    {
       int id = buf[pos++];
 
       // parse header value
@@ -199,10 +199,10 @@ public class DaspMessage
       byte[] bytes = null;
       switch (id & 0x3)
       {
-        case 0: 
+        case 0:
           break;
         case 1:
-          u2 = ((buf[pos++] & 0xff) << 8) | (buf[pos++] & 0xff); 
+          u2 = ((buf[pos++] & 0xff) << 8) | (buf[pos++] & 0xff);
           break;
         case 2:
           int s = pos;
@@ -210,12 +210,13 @@ public class DaspMessage
           str = new String(buf, s, pos-s-1, "UTF-8");
           break;
         case 3:
-          int blen = buf[pos++] & 0xff; 
+          int blen = buf[pos++] & 0xff;
           bytes = blen == 0 ? noBytes : new byte[blen];
-          System.arraycopy(buf, pos, bytes, 0, blen);  
-          break;     
+          System.arraycopy(buf, pos, bytes, 0, blen);
+          pos += blen;
+          break;
       }
-      
+
       // set header field
       switch (id)
       {
@@ -226,21 +227,21 @@ public class DaspMessage
         case USERNAME:         username        = str;   break;
         case DIGEST:           digest          = bytes; break;
         case IDEAL_MAX:        idealMax        = u2;    break;
-        case ABS_MAX:          absMax          = u2;    break; 
+        case ABS_MAX:          absMax          = u2;    break;
         case ACK:              ack             = u2;    break;
         case ACK_MORE:         ackMore         = bytes; break;
         case RECEIVE_MAX:      receiveMax      = u2;    break;
-        case RECEIVE_TIMEOUT:  receiveTimeout  = u2 * 1000L; break; 
+        case RECEIVE_TIMEOUT:  receiveTimeout  = u2 * 1000L; break;
         case ERROR_CODE:       errorCode       = u2;    break;
       }
-    }          
-    
-    // payload is whatever is left (we need to 
+    }
+
+    // payload is whatever is left (we need to
     // make a copy  since  input buffer is reused)
     len = len - pos;
     this.payload = len == 0 ? noBytes : new byte[len];
     System.arraycopy(buf, pos, this.payload, 0, len);
-    
+
     return this;
   }
 
@@ -251,9 +252,9 @@ public class DaspMessage
   public void dump() { dump(new PrintWriter(System.out)); }
 
   public void dump(PrintWriter out)
-  {                            
+  {
     out.println("sessionId=" + sessionId + " msgType=" + msgType + " seqNum=" + seqNum);
-    
+
     if (version != -1)           out.println("  version         = " + version);
     if (remoteId != -1)          out.println("  remoteId        = " + remoteId);
     if (digestAlgorithm != null) out.println("  digestAlgorithm = " + digestAlgorithm);
@@ -263,13 +264,13 @@ public class DaspMessage
     if (idealMax != -1)          out.println("  idealMax        = " + idealMax);
     if (absMax != -1)            out.println("  absMax          = " + absMax);
     if (ack != -1)               out.println("  ack             = " + ack);
-    if (ackMore != null)         out.println("  ackMore         = " + ackMore);
+    if (ackMore != null)         out.println("  ackMore         = " + toString(ackMore));
     if (receiveMax != -1)        out.println("  receiveMax      = " + receiveMax);
     if (receiveTimeout != -1)    out.println("  receiveTimeout  = " + receiveTimeout);
     if (errorCode != -1)         out.println("  errorCode       = " + errorCode);
-    
+
     out.flush();
-  }                               
+  }
 
   static String toString(byte[] buf) { return toString(buf, buf.length); }
   static String toString(byte[] buf, int len)
@@ -280,7 +281,7 @@ public class DaspMessage
     s.append(']');
     return s.toString();
   }
-              
+
 ////////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////////
@@ -290,8 +291,8 @@ public class DaspMessage
   // 5 byte header
   int sessionId = -1;
   int msgType = -1;
-  int seqNum  = -1;      
-  
+  int seqNum  = -1;
+
   // header fields
   int version = -1;
   int remoteId = -1;
@@ -307,12 +308,12 @@ public class DaspMessage
   long receiveTimeout = -1;   // ms (encoded as sec)
   int errorCode = -1;
 
-  // payload bytes  
-  byte[] payload = noBytes;    
-  
+  // payload bytes
+  byte[] payload = noBytes;
+
   // next in Queue linked list
   DaspMessage next;
-  
+
   // DaspSession
   DaspSession session;
 }
