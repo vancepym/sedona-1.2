3f24512aa228ef6bb237f2d4fd5b359d62c7bfa2
Scott Fan <fancp2007@gmail.com>
2017-07-10 11:19:57 +0800

    datetimeStd: Fix doSetClock() native implementation on unix platform.

--- src/kits/datetimeStd/native/std/datetimeStd_DateTimeServiceStd.c
+++ src/kits/datetimeStd/native/std/datetimeStd_DateTimeServiceStd.c
@@ -35,48 +35,53 @@ int64_t datetimeStd_DateTimeServiceStd_doNow(SedonaVM* vm, Cell* params)
   time_t now = time(NULL);
   int64_t nanos;
   now -= SEDONA_EPOCH_OFFSET_SECS;
-  nanos = ((int64_t) now * 1000 * 1000 *1000);
+  nanos = ((int64_t) now * 1000 * 1000 * 1000);
   return nanos;
 }
 
- //  Setting system time not implemented on Win32
-  
-// void doSetClock 
+// void doSetClock
 Cell datetimeStd_DateTimeServiceStd_doSetClock(SedonaVM* vm, Cell* params)
 {
   int64_t nanos = *(int64_t*)(params+0); // param 0+1
-    
+
+#ifdef _WIN32
+  // TODO: Setting the System Time not implemented on Win32
+#else
+  time_t new_time = (time_t)(nanos / 1000 / 1000 / 1000);
+  new_time += SEDONA_EPOCH_OFFSET_SECS;
+  stime(&new_time);
+#endif
+
   return nullCell;
 }
 
 Cell datetimeStd_DateTimeServiceStd_doGetUtcOffset(SedonaVM* vm, Cell* params)
 {
   Cell result;
-   
-  //  Per Microsoft CRT docs, timezone global variable is updated whenever 
+
+  //  Per Microsoft CRT docs, timezone global variable is updated whenever
   //  localtime is called.  QNX follows same convention.
-  //  timezone is difference between localtime and GMT, so we must negate to 
+  //  timezone is difference between localtime and GMT, so we must negate to
   //  get utcOffset as we define it.
   time_t now;
   struct tm *lcltime;
 
   tzset();   // set up time zone related variables before calling localtime()
-  
+
   now     = time(NULL);
   lcltime = localtime(&now);
 
   #ifdef _WIN32
+    //printf("_timezone %ld, _daylight %d, result.ival %d\n", _timezone, _daylight, result.ival);
     result.ival = -(int)_timezone;
     if (_daylight)
       result.ival += 3600;  //  if daylight savings active, advance one hour
   #else
+    //printf("timezone %ld, daylight %d, result.ival %d\n", timezone, daylight, result.ival);
     result.ival = -(int)timezone;
     if (daylight)
       result.ival += 3600;  //  if daylight savings active, advance one hour
   #endif
 
-//printf("timezone %lld result.ival %d\n", _timezone, result.ival);
-
   return result;
-  
-}  
+}
